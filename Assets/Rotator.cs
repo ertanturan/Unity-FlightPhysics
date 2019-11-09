b0c80a38960a10a7d09fb63d0ca6589319d5c937
Ertan Turan <ertanturan@outlook.com>
2019-11-09 12:53:58 +0300

    models added

---
+++ Assets/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 Euler;
+    public float Angle;
+    void Update()
+    {
+        transform.Rotate(Euler, Angle);
+    }
+}
