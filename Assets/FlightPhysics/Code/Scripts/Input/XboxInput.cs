472e552b2990e0867c18e963d8241da75fbeafd7
Ertan Turan <ertanturan@outlook.com>
2019-11-22 12:29:36 +0300

    Base classes designed and stubbed.

---
+++ Assets/FlightPhysics/Code/Scripts/Input/XboxInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XboxInput : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
