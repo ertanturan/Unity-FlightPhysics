5bf3693917e656e0f1183488b0f87437260409a5
Ertan Turan <ertanturan@outlook.com>
2019-11-25 14:06:13 +0300

    TEST : thrust force coding tests.

---
+++ Assets/FlightPhysics/Code/Tests/RigidbodyTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyTest : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 force = transform.up * Physics.gravity.magnitude * _rb.mass;
+        _rb.AddForce(force);
+        Debug.DrawRay(transform.position, force, Color.green);
+    }
+}
