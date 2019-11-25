7fa309f88bac058eef4721e170c2525022b65630
Ertan Turan <ertanturan@outlook.com>
2019-11-25 10:06:54 +0300

    xbox inputs finished and tested , inputmanager set for xbox

--- Assets/FlightPhysics/Code/Scripts/Input/XboxInput.cs
+++ Assets/FlightPhysics/Code/Scripts/Input/XboxInput.cs
@@ -10,19 +10,19 @@
         {
             pitch = Input.GetAxis("Vertical");
             roll = Input.GetAxis("Horizontal");
-            yaw = Input.GetAxis("Yaw");
-            throttle = Input.GetAxis("Throttle");
+            yaw = Input.GetAxis("X_RH_Stick");
+            throttle = Input.GetAxis("X_RV_Stick");
 
             //Brakes
             brake = Input.GetAxis("Fire1");
 
             //Flaps
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetButtonDown("X_R_Bumper"))
             {
                 flaps += 1;
             }
 
-            if (Input.GetKeyDown(KeyCode.G))
+            if (Input.GetButtonDown("X_L_Bumper"))
             {
                 flaps -= 1;
             }
