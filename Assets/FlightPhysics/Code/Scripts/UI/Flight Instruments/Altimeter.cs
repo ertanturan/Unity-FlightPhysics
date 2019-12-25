6cc540afe4d4ab4390a5d8c98aaee70cef4103cf
Ertan Turan <ertanturan@outlook.com>
2019-12-25 21:26:57 +0300

    altimeter finished

---
+++ Assets/FlightPhysics/Code/Scripts/UI/Flight Instruments/Altimeter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FlightPhysics;
+using UnityEngine;
+
+public class Altimeter : MonoBehaviour
+{
+    [Header("Altimeter Properties")]
+    public FlightController Controller;
+    public RectTransform HunderdsPointer;
+    public RectTransform ThousandsPointer;
+
+    public void Update()
+    {
+        if (Controller)
+        {
+            float currentAlt = Controller.CurrentMSL;
+            float currentThousands = currentAlt / 1000f;
+            currentThousands = Mathf.Clamp(currentThousands, 0f, 10f);
+
+            float currentHundreds = currentAlt - (Mathf.Floor(currentThousands));
+            currentHundreds = Mathf.Clamp(currentHundreds, 0f, 1000);
+
+
+            if (ThousandsPointer)
+            {
+                float normalizedThousands = Mathf.InverseLerp(0f, 10f, currentThousands);
+                float thousandsRotation = normalizedThousands * 360;
+
+                ThousandsPointer.rotation = Quaternion.Euler(0f, 0f, -thousandsRotation);
+            }
+
+            if (HunderdsPointer)
+            {
+                float normalizedHunderds = Mathf.InverseLerp(0f, 1000, currentHundreds);
+                float hundredsRotation = 360f * normalizedHunderds;
+
+                HunderdsPointer.rotation = Quaternion.Euler(0f, 0f, -hundredsRotation);
+            }
+
+        }
+    }
+
+}
