d77b38d14973a331421651e1febfd214675e49cf
Ertan Turan <ertanturan@outlook.com>
2019-12-25 19:24:49 +0300

    Window Kit imported.

---
+++ Assets/FlightPhysics/Code/Scripts/UI/WindowUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CanvasGroup))]
+[RequireComponent(typeof(Animator))]
+public class WindowUI : MonoBehaviour
+{
+
+    [SerializeField]
+    protected Animator animator;
+
+    public bool IsShown
+    {
+        get
+        {
+            return animator.GetBool("Shown");
+        }
+        set
+        {
+            if (value != IsShown)
+            {
+                if (value)
+                    onShowing.Invoke();
+                else
+                    onHiding.Invoke();
+            }
+            animator.SetBool("Shown", value);
+        }
+    }
+
+    public bool startHidden = true;
+
+    public UnityEvent onShowing;
+    public UnityEvent onHiding;
+
+    protected virtual void Start()
+    {
+        if (!startHidden)
+            Show();
+    }
+
+    public void Show()
+    {
+        IsShown = true;
+    }
+
+    public void Hide()
+    {
+        IsShown = false;
+    }
+}
