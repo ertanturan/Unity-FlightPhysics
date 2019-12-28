5393dd36b395bdf14cb9513ffee2511934917986
Ertan Turan <ertanturan@outlook.com>
2019-12-28 23:07:59 +0300

    Throttle Lever added.

--- Assets/FlightPhysics/Code/Scripts/UI/Flight Instruments/Altimeter.cs
+++ Assets/FlightPhysics/Code/Scripts/UI/Flight Instruments/Altimeter.cs
@@ -1,45 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using FlightPhysics;
-using FlightPhysics.UI;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Altimeter : MonoBehaviour, IAirplaneUI
+namespace FlightPhysics.UI
 {
-    [Header("Altimeter Properties")]
-    public FlightController Controller;
-    public RectTransform HunderdsPointer;
-    public RectTransform ThousandsPointer;
 
-    public void HandleAirplaneUI()
+    public class Altimeter : MonoBehaviour, IAirplaneUI
     {
-        if (Controller)
+        [Header("Altimeter Properties")]
+        public FlightController Controller;
+        public RectTransform HunderdsPointer;
+        public RectTransform ThousandsPointer;
+
+        public void HandleAirplaneUI()
         {
-            float currentAlt = Controller.CurrentMSL;
-            float currentThousands = currentAlt / 1000f;
-            currentThousands = Mathf.Clamp(currentThousands, 0f, 10f);
-
-            float currentHundreds = currentAlt - (Mathf.Floor(currentThousands));
-            currentHundreds = Mathf.Clamp(currentHundreds, 0f, 1000);
-
-
-            if (ThousandsPointer)
+            if (Controller)
             {
-                float normalizedThousands = Mathf.InverseLerp(0f, 10f, currentThousands);
-                float thousandsRotation = normalizedThousands * 360;
+                float currentAlt = Controller.CurrentMSL;
+                float currentThousands = currentAlt / 1000f;
+                currentThousands = Mathf.Clamp(currentThousands, 0f, 10f);
 
-                ThousandsPointer.rotation = Quaternion.Euler(0f, 0f, -thousandsRotation);
+                float currentHundreds = currentAlt - (Mathf.Floor(currentThousands));
+                currentHundreds = Mathf.Clamp(currentHundreds, 0f, 1000);
+
+
+                if (ThousandsPointer)
+                {
+                    float normalizedThousands = Mathf.InverseLerp(0f, 10f, currentThousands);
+                    float thousandsRotation = normalizedThousands * 360;
+
+                    ThousandsPointer.rotation = Quaternion.Euler(0f, 0f, -thousandsRotation);
+                }
+
+                if (HunderdsPointer)
+                {
+                    float normalizedHunderds = Mathf.InverseLerp(0f, 1000, currentHundreds);
+                    float hundredsRotation = 360f * normalizedHunderds;
+
+                    HunderdsPointer.rotation = Quaternion.Euler(0f, 0f, -hundredsRotation);
+                }
+
             }
-
-            if (HunderdsPointer)
-            {
-                float normalizedHunderds = Mathf.InverseLerp(0f, 1000, currentHundreds);
-                float hundredsRotation = 360f * normalizedHunderds;
-
-                HunderdsPointer.rotation = Quaternion.Euler(0f, 0f, -hundredsRotation);
-            }
-
         }
+
     }
 
 }
---
+++ Assets/FlightPhysics/Code/Scripts/UI/Flight Instruments/ThrottleLever.cs
@@ -0,0 +1,27 @@
+﻿using FlightPhysics.Input;
+using UnityEngine;
+
+namespace FlightPhysics.UI
+{
+    public class ThrottleLever : MonoBehaviour, IAirplaneUI
+    {
+        [Header("Throttle Lever")]
+        public BaseFlightInput Input;
+        public RectTransform ParentRect;
+        public RectTransform HandleRect;
+        public float HandleSpeed = 2f;
+
+        public void HandleAirplaneUI()
+        {
+            if (Input && HandleRect && ParentRect)
+            {
+                float height = ParentRect.rect.height;
+                Vector2 targetPos = new Vector2(0f, height * Input.StickyThrottle);
+                HandleRect.anchoredPosition = Vector2.Lerp(HandleRect.anchoredPosition,
+                    targetPos, Time.deltaTime * HandleSpeed);
+            }
+        }
+    }
+
+
+}
