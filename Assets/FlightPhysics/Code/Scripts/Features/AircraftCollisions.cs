90dbbe5427971ca20aaf1b71e7e261b38fc54cf7
Ertan Turan <ertanturan@outlook.com>
2019-12-29 13:49:59 +0300

    Collision normals and points calculated .

---
+++ Assets/FlightPhysics/Code/Scripts/Features/AircraftCollisions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlightPhysics
+{
+    public class AircraftCollisions : MonoBehaviour
+    {
+        private List<Vector3> _hitNormals = new List<Vector3>();
+        private List<Vector3> _hitPoints = new List<Vector3>();
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            for (int i = 0; i < collision.contacts.Length; i++)
+            {
+                _hitPoints.Add(collision.contacts[i].point);
+                _hitNormals.Add(collision.contacts[i].normal);
+            }
+        }
+    }
+}
+
