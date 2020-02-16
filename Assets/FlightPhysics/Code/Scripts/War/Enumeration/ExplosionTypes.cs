227201ee5526285243e07184270e2aa26d930bf9
Ertan Turan <ertanturan@outlook.com>
2020-02-16 12:40:00 +0300

    initializing explosion system

---
+++ Assets/FlightPhysics/Code/Scripts/War/Enumeration/ExplosionTypes.cs
@@ -0,0 +1,5 @@
+﻿public enum ExplosionTypes
+{
+    GroundExplosion,
+    MassiveExplosion
+}
---
+++ Assets/FlightPhysics/Code/Scripts/War/Explosives/Explosive.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Explosive : MonoBehaviour
+{
+    public float ExplosionRange = 10f;
+    public float Damage = 20f;
+    public ExplosionTypes Type;
+
+}
