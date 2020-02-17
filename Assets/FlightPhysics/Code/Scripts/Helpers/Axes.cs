8f61107cfae0ce9e9f5000f1849d43b54cac4fdd
Ertan Turan <ertanturan@outlook.com>
2020-02-17 21:32:56 +0300

    helper added

---
+++ Assets/FlightPhysics/Code/Scripts/Helpers/Axes.cs
@@ -0,0 +1,6 @@
+﻿public enum Axes
+{
+    X,
+    Y,
+    Z
+}
---
+++ Assets/FlightPhysics/Code/Scripts/Helpers/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Axes Axes;
+    public float RotationSpeed = 5f;
+    void FixedUpdate()
+    {
+        switch (Axes)
+        {
+            case Axes.X:
+                transform.Rotate();
+                break;
+            case Axes.Y:
+                break;
+            case Axes.Z:
+                break;
+        }
+    }
+
+
+}
+
