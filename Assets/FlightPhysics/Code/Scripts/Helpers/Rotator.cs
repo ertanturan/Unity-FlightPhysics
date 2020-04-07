4abd4bb1babe4e4365768b02a3c1319de9e84836
Ertan Turan <ertanturan@outlook.com>
2020-04-07 19:18:14 +0300

    rotator script commented

--- Assets/FlightPhysics/Code/Scripts/Helpers/Rotator.cs
+++ Assets/FlightPhysics/Code/Scripts/Helpers/Rotator.cs
@@ -4,19 +4,19 @@ public class Rotator : MonoBehaviour
 {
     public Axes Axes;
     public float RotationSpeed = 5f;
-    void FixedUpdate()
-    {
-        switch (Axes)
-        {
-            case Axes.X:
-                transform.Rotate();
-                break;
-            case Axes.Y:
-                break;
-            case Axes.Z:
-                break;
-        }
-    }
+    //void FixedUpdate()
+    //{
+    //    switch (Axes)
+    //    {
+    //        case Axes.X:
+    //            transform.Rotate();
+    //            break;
+    //        case Axes.Y:
+    //            break;
+    //        case Axes.Z:
+    //            break;
+    //    }
+    //}
 
 
 }
