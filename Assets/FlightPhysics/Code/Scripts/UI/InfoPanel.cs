81891423bd25cd3e879eef87ccabf71aee1d8124
Ertan Turan <ertanturan@outlook.com>
2020-04-17 23:12:36 +0300

    UI Infos filled

---
+++ Assets/FlightPhysics/Code/Scripts/UI/InfoPanel.cs
@@ -0,0 +1,29 @@
+﻿using FlightPhysics.Input;
+using TMPro;
+using UnityEngine;
+
+public class InfoPanel : MonoBehaviour
+{
+    [Header("Text")]
+    public TextMeshProUGUI Pitch;
+    public TextMeshProUGUI Yaw;
+    public TextMeshProUGUI Roll;
+    public TextMeshProUGUI Throttle;
+    public TextMeshProUGUI StickyThrottle;
+    public TextMeshProUGUI Flaps;
+    public TextMeshProUGUI Brake;
+
+    [Header("Input")]
+    public BaseFlightInput Input;
+
+    private void Update()
+    {
+        Pitch.text = Input.Pitch.ToString();
+        Yaw.text = Input.Yaw.ToString();
+        Roll.text = Input.Yaw.ToString();
+        Throttle.text = Input.Throttle.ToString();
+        StickyThrottle.text = Input.StickyThrottle.ToString();
+        Flaps.text = Input.Flaps.ToString();
+        Brake.text = Input.Brake.ToString();
+    }
+}
